5e48843ad84d1eb19298bdc688eb966921dd3555
rerkki <rerkki@gmail.com>
2025-03-31 15:09:47 +0300

    Update mqtt_mongo.js

--- mqtt_mongo.js
+++ mqtt_mongo.js
@@ -1,55 +1,48 @@
 
+//MQTT-välityspalvelimen määrittely
 const mqtt    = require('mqtt');
-const broker = 'mqtt://broker.hivemq.com:1883';
+const broker = 'mqtt://test.mosquitto.org';
 const user = '';
-const pw = '';
+const pw = ''; 
 
+//määritellään välityspalvelimen "olio"
 mq = mqtt.connect(broker, {
   'username': user,
   'password': pw
 });
 
-const MongoClient = require('mongodb').MongoClient;
+//tilataan oikea topic
+mq.subscribe('automaatio/#');
 
-var myCollection;
-var db;
-var client_m;
-var obj;
-
-
-mq.on('message', function(topic, message) {
-  console.log(message.toString('utf8'));
-  obj = JSON.parse(message);
-  console.log(obj.Time, obj.T, obj.H);
-	createConnection(function(){
-    		addDocument(function(){
-		});
-	});
-});
-
+//liitytään välityspalvelimeen
 mq.on('connect', function(){
     console.log('Connected.....');
 });
 
-mq.subscribe('automaatio/#');
+//Määritellään tietokanta-API
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://eki:eki@cluster0.91fze.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; //korvaa tämä omalla URI:lla
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-function addDocument(onAdded){
-			myCollection.insert(obj, function(err, result) {
-        if(err)
-            throw err;
-        console.log("entry saved");
-        onAdded();
-    	});
-}
+//määritellään tietokannan ja kokoelman nimi sekä dataobjekti sensoridatan käsittelyyn
+const myDB = client.db("sensordata2");
+const myColl = myDB.collection("sensordata2");
+var obj;
 
-function createConnection(onCreate){
-    MongoClient.connect('mongodb://username:password@cluster0-shard-00-00-xxxxx.gcp.mongodb.net:27017,cluster0-shard-00-01-fn7gd.gcp.mongodb.net:27017,cluster0-shard-00-02-fn7gd.gcp.mongodb.net:27017/DBNAME?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', function(err, client_m) {
-        db = client_m.db('DBNAME'); // DB name
-					if(err)
-            throw err;
-        console.log("connected to the mongoDB !");
-        myCollection = db.collection('COLLECTION NAME'); //Collection name
-        onCreate();
-	    client_m.close();
-    });
-}
+//odotetaan dataa välityspalvelimelta ja viedään data tietokantaan
+mq.on('message', function(topic, message) {
+  console.log(message.toString('utf8'));
+  obj = JSON.parse(message);
+  console.log(obj.Time, obj.T, obj.H, obj.DP, obj.pCount);
+	myColl.insertOne(obj);
+	console.log(
+	`An entry was inserted successfully`,
+	);
+});
+
